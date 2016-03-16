c7ce0c5e3cb741199508f7a588809372d00bbebb
calimanleontin <calimanleontin@gmail.com>
2016-03-16 09:48:04 +0200

    tag service

---
+++ public/js/services/tagService.js
@@ -0,0 +1,22 @@
+angular.module('tagService', [])
+
+    .factory('Tag', function ($http){
+        return {
+            get : function(){
+                return $http.get('/api/tags');
+            },
+
+            save : function(tagData){
+                return $http({
+                    method  :   'POST',
+                    url     :   '/api/tags/save',
+                    data    : $.param(tagData),
+                    headers : {'Content-Type' : 'application/x-www-form-urlencoded'}
+                });
+            },
+
+            destroy : function(id) {
+                return $http.delete('/api/tags/' + id);
+            }
+        }
+    });
