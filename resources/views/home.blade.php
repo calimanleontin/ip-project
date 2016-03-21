f172be1680313ff834ab793c82498916aa3f2963
calimanleontin <calimanleontin@gmail.com>
2016-03-21 22:54:28 +0200

    show map on modal

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -18,21 +18,45 @@
                             <a href="/company/{{ $company->slug }}"> View {{ $company->name }} web page.</a>
                         </li>
                         <li>
-                            <btn class="btn btn-default" onClick="showMap({{ $company->id }})">Show map</btn>
+                            <btn class="btn btn-default" data-toggle="modal" data-target="#myModal" onClick="showMap({{ $company->id }})">Show map</btn>
                         </li>
                     </ul>
                 </div>
             @endforeach
         </div>
-        <button id="calcRoute">Calc route</button>
 
-        <div id="map-canvas"></div>
-        <div id="overlay">
-            <div id="overlayContent">
+        <!-- Modal -->
+        <div class="modal fade" id="myModal" role="dialog">
+            <div class="modal-dialog">
 
+                <!-- Modal content-->
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal">&times;</button>
+                        <h4 class="modal-title">My map</h4>
+                    </div>
+                    <div class="modal-body ">
+
+                        <button id="calcRoute">Calc route</button>
+
+                        <div id="map-canvas"></div>
+                        <div id="overlay">
+                            <div id="overlayContent">
+
+
+                            </div>
+                        </div>
+
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                    </div>
+                </div>
 
             </div>
         </div>
+
+
     <script src="/js/myLocation.js"></script>
     </div>
 
