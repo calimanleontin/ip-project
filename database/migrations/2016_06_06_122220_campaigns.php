bba8f0decebf30481d8aca12420ffdde2edcd6b6
Belu Octavian <belu.octavian@yahoo.com>
2016-06-06 15:28:59 +0300

    Campaign migration

---
+++ database/migrations/2016_06_06_122220_campaigns.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Campaigns extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('campaigns', function(Blueprint $table){
+            $table->increments('id');
+            $table->integer('company_id')->unsigned()->default(0);
+            $table->foreign('company_id')
+              ->references('id')
+              ->on('companies')
+              ->onDelete('cascade');
+            $table->date('start');
+            $table->date('end');
+            $table->longText('winners');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('campaigns');
+    }
+}
