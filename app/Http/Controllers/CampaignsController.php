5d9700efb2fb8b6e62a56df42523d407ae4cb464
Belu Octavian <belu.octavian@yahoo.com>
2016-06-06 16:00:40 +0300

    Create and view campaign

--- app/Http/Controllers/CampaignsController.php
+++ app/Http/Controllers/CampaignsController.php
@@ -5,10 +5,28 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
+use App\Campaign;
 
 class CampaignsController extends Controller
 {
   function getCreate(Request $request) {
     return view('campaigns/create');
+  }
+
+  function postCreate(Request $request) {
+    $user = \Auth::user();
+    $company = $user->company;
+    $campaign = new Campaign;
+    $campaign->company_id = $company->id;
+    $campaign->title = $request->get('title');
+    $campaign->start = $request->get('start_date');
+    $campaign->end = $request->get('end_date');
+    $campaign->save();
+    return redirect('campaigns/' . $campaign->getAttribute('id'))->with('success', 'Ati creat campania cu succes!');
+  }
+
+  function viewEntity($id, Request $request) {
+    $campaign = Campaign::find($id);
+    return view('campaigns/viewEntity')->with('campaign', $campaign);
   }
 }
---
+++ resources/views/campaigns/viewEntity.blade.php
@@ -0,0 +1,24 @@
+@extends('app')
+@section('title')
+    View campaign
+@endsection
+
+@section('content')
+    <div class="panel-body">
+        <div>Campaign: {{ $campaign->title }}</div>
+        <div>Start date: {{ $campaign->start }}</div>
+        <div>End date: {{ $campaign->end }}</div>
+        @if (!empty($winners))
+            <div>
+                <h5>Winners:</h5>
+                <ul>
+                    @foreach ($winners as $key => $name)
+                        <li>{{ $key }}. {{ $name }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+    </div>
+@endsection
+
+
